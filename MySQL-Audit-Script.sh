8a8697a1403f408af1f865b8df9257336330fbbd
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2020-07-30 04:17:51 +0200

    Create MySQL-Audit-Script.sh

---
+++ MySQL-Audit-Script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#----------------------------------------------------------------------------------------------------------------------------------------
+# Basic script to dump the table "mysql.user" that contains the list of dataabse users, password hashes, privileges...
+# To run this script you need to do the following 2 steps:
+# Log into the Linux server hosting the MySQL database
+# Run the "MySQL-Audit-Script.sh" and type in the MySQL root's password when requested
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+mysqldump -u root -p -h localhost mysql user > MySQL_USER_table_dump.sql
+gzip MySQL_USER_table_dump.sql
+
+# mysqldump --opt --user=root --password --all-databases > MySQL_all_databases_dump.sql
+# gzip MySQL_all_databases_dump.sql
